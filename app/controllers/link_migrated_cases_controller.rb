32cee7d0c8de8427d4962ff539129d6a51141397
Leandro Costantini <leandro.costantini@justice.gov.uk>
2026-08-04 11:58:48 +0100

    [ACD-1214] Populate the Needs Linking tab on the Link Migrated Cases page

--- app/controllers/link_migrated_cases_controller.rb
+++ app/controllers/link_migrated_cases_controller.rb
@@ -8,10 +8,10 @@ class LinkMigratedCasesController < ApplicationController
                         court_name mode_of_trial reason_for_man_linking maat_id
                         defendant_date_of_birth linked_at linked_by].freeze
   CASES_PER_PAGE = 10
-  TABS = %w[need_linking pending manually_linked auto_linked].freeze
+  TABS = %w[action_required pending manually_linked auto_linked].freeze
   COLUMNS = {
-    'need_linking' => %w[case_urn defendant_name xhibit_case_number court_name mode_of_trial
-                         reason_for_man_linking maat_id],
+    'action_required' => %w[case_urn defendant_name xhibit_case_number court_name mode_of_trial
+                            reason_for_man_linking],
     'pending' => %w[case_urn defendant_name xhibit_case_number court_name mode_of_trial],
     'manually_linked' => %w[case_urn_new_tab maat_id defendant_name
                             defendant_date_of_birth linked_at linked_by],
