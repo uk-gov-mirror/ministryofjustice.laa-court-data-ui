92a34b110ef1fff05c2c5e22f143a5e46b1ad2d1
Leandro Costantini <leandro.costantini@justice.gov.uk>
2026-08-14 13:43:41 +0100

    [ACD-1489] Add link XHIBIT migrated case page

--- app/helpers/link_migrated_cases_helper.rb
+++ app/helpers/link_migrated_cases_helper.rb
@@ -32,15 +32,13 @@ module LinkMigratedCasesHelper
   end
 
   def formatted_process_errors(process_errors)
-    return process_errors unless process_errors.is_a?(Hash)
+    return process_errors unless process_errors.as_json.is_a?(Hash)
 
     formatted_values = process_errors.with_indifferent_access.slice(:error, :message).values.compact_blank
     formatted_values.join(" - ").presence || process_errors.to_s
   end
 
-  def link_maat_id_url(defendant_id, case_urn)
-    # TODO: Replace this link when the "Link court data" page is created
-    # Using Defendant Link MAAT ID page instead for now
+  def link_maat_id_url(id)
     link_to("Link MAAT ID",
             link_defendant_path(defendant_id, urn: case_urn),
             class: "govuk-link govuk-link--no-visited-state")
